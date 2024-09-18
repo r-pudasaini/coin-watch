5e3746ff7985df2df13aba37409ed45f25862600
Riyos_P <riyosp678@vt.edu>
2024-09-18 16:02:02 -0400

    Added search bar It can filter over coins that match the search query
    TODO:
      1. Make sure default page renders properly
      2. make sure the page only displays 20 items at a time
      3. Add the ability to load more entries per page.

--- crypto-app/src/components/Coins.js
+++ crypto-app/src/components/Coins.js
@@ -1,15 +1,43 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import CoinItem from './CoinItem'
 import Coin from '../routes/Coin'
 import './Coins.css'
 import {Link} from 'react-router-dom'
+import {FaSearch} from 'react-icons/fa'
 
 const Coins = (props) =>
 {
 
+  const [search, setSearch] = useState("")
+  const [coins2show, setCoins2show] = useState(props.coins)
+
+  const updateSearch = (value) => {
+    setSearch(value)
+  }
+
+  const handleClick = () => {
+    // now when we click the button, we need to parse all the elements in props, and filter the ones that 
+    // do not have the search word within them.
+
+    let arr = props.coins.filter((coin) => coin.id.includes(search) )
+    setCoins2show(arr)
+  }
+
   return (
     <div className='container'>
       <div>
+        <div className='input-wrapper'>
+          <input
+            placeholder="Search for a coin..."
+            value={search}
+            onChange={(e) => updateSearch(e.target.value) }
+          />
+
+          <FaSearch
+            id="search-icon"
+            onClick={handleClick}
+          />
+        </div>
 
         <div className='heading'>
           <p>#</p>
@@ -20,7 +48,7 @@ const Coins = (props) =>
           <p className='hide-mobile'>Market Cap</p>
         </div>
 
-        {props.coins.map(coin => {
+        {coins2show.map(coin => {
             return (
 
               <Link to={`/coin-watch/${coin.id}`} element={<Coin />} key={coin.id}>
