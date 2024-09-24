2b0c1f603b367298b3fedd1cc4910880e45bbc75
Riyos_P <riyosp678@vt.edu>
2024-09-24 10:03:50 -0400

    Revert "Made some basic pagination additions"
    This reverts commit fdd6f102cefde2d8a9cc5235e4b63000b018808f.
    Now, the main branch has only the search bar, and a future load more button

--- crypto-app/src/App.js
+++ crypto-app/src/App.js
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from 'react'
+import axios from 'axios'
 import Coins from './components/Coins'
 import Navbar from './Navbar'
 import {Routes, Route} from 'react-router-dom'
@@ -7,13 +8,29 @@ import Coin from './routes/Coin'
 
 function App() {
 
+  const [coins, setCoins] = useState([])
+  const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&per_page=250'
+
+
+  // 1. query every single coin that coingecko has
+  // 2. pass the result of that query into the Coins element
+
+
+  useEffect(() =>
+  {
+      axios.get(url).then((response) => {
+        setCoins(response.data);
+      }).catch((error) => {
+        console.log(error);
+      })
+  }, [url])
 
   return (
     <>
       <Navbar />
       <Routes>
-        <Route path='/' element={<Coins/>} />
-        <Route path='/coin-watch' element={<Coins/>} />
+        <Route path='/' element={<Coins coins={coins} />} />
+        <Route path='/coin-watch' element={<Coins coins={coins} />} />
         <Route path='/coin-watch' element={<Coin/>}>
           <Route path=':coinId' element={<Coin />} />
         </Route>
--- crypto-app/src/components/Coins.js
+++ crypto-app/src/components/Coins.js
@@ -4,33 +4,13 @@ import Coin from '../routes/Coin'
 import './Coins.css'
 import {Link} from 'react-router-dom'
 import {FaSearch} from 'react-icons/fa'
-import axios from 'axios'
-import { FiChevronsLeft, FiChevronLeft, FiChevronRight, FiChevronsRight  } from "react-icons/fi";
-
 
 const Coins = (props) =>
 {
 
-  let allPages = 100
-
-  const [coins, setCoins] = useState([])
   const [search, setSearch] = useState("")
+  const [coins2show, setCoins2show] = useState(props.coins)
   const [button, setButton] = useState(false)
-  const [coinsPerPage, setCoinsPerPage] = useState(40)
-  const [pageNum, setPageNum] = useState(1)
-
-
-  useEffect(() =>
-  {
-    let url = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&per_page=${coinsPerPage}&page=${pageNum}`
-      axios.get(url).then((response) => {
-        const c = response.data.filter((coin) => coin.id.includes(search))
-        setCoins(c)
-      }).catch((error) => {
-        console.log(error)
-      })
-  }, [coinsPerPage, pageNum, button])
-
 
   const updateSearch = (value) => {
     setSearch(value)
@@ -43,13 +23,25 @@ const Coins = (props) =>
   const handleKey = (event) => {
     if (event.key === "Enter")
     {
-      setButton(!button)
+      handleClick()
     }
   }
 
+  useEffect(() => {
+
+    let arr = props.coins.filter((coin) => coin.id.includes(search))
+
+    if (arr.length > 20)
+    {
+      arr = arr.slice(0, 20)
+    }
+
+    setCoins2show(arr)
+
+  }, [button, props])
+
   return (
     <div className='container'>
-
       <div>
         <div className='input-wrapper'>
           <input
@@ -65,38 +57,6 @@ const Coins = (props) =>
           />
         </div>
 
-        <div className="pagination">
-
-          <div className="elements-per-page">
-            <div className="elements-view"> 
-              Showing <span>{coinsPerPage}</span> coins per-page
-                <ul className="elements-dropdown">
-                  <li> 5 </li>
-                  <li> 10 </li>
-                  <li> 20 </li>
-                  <li> 50 </li>
-                  <li> 100 </li>
-                </ul>
-            </div>
-          </div>
-
-          <div className="change-page-number">
-            <FiChevronsLeft />
-            <FiChevronLeft className="margin-image"/>
-
-            <input
-              placeholder={pageNum + "/" + allPages}
-              value={search}
-              onChange={(e) => updateSearch(e.target.value) }
-              onKeyPress={(e) => handleKey(e)}
-            />
-
-            <FiChevronRight className="margin-image"/>
-            <FiChevronsRight className="margin-image"/>
-          </div>
-
-        </div>
-
         <div className='heading'>
           <p>#</p>
           <p className='coin-name'>Coin</p>
@@ -106,20 +66,13 @@ const Coins = (props) =>
           <p className='hide-mobile'>Market Cap</p>
         </div>
 
-        {/*        {coins.map(coin => {
+        {coins2show.map(coin => {
             return (
 
+              <Link to={`/coin-watch/${coin.id}`} element={<Coin />} key={coin.id}>
+                <CoinItem coin={coin}/>
+              </Link>
             )
-        })}
-
-        */}
-
-        {coins.map(coin => {
-          return (
-            <Link to={`/coin-watch/${coin.id}`} element={<Coin />} key={coin.id}>
-              <CoinItem coin={coin}/>
-            </Link>
-          )
         })}
 
       </div>
