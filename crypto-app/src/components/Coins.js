3327730e150fc9c50a6e0c6f26b95003be978a1b
Riyos_P <riyosp678@vt.edu>
2024-09-24 09:57:49 -0400

    Revert "The per-pager and page selector seem to work"
    This reverts commit ee29b15c0af592f71d61b7f978ef3946c48a2034.

--- crypto-app/src/components/Coins.js
+++ crypto-app/src/components/Coins.js
@@ -13,19 +13,18 @@ const Coins = (props) =>
 
   let allPages = 100
 
-  const [coins, setCoins] = useState([])  // the coin data we pass to the Coin route
-  const [coinSearch, setCoinSearch] = useState("")  // filters which coins we fetch for the client.
-  const [pageSearch, setPageSearch] = useState("")  // filters which coins we fetch for the client.
-  const [button, setButton] = useState(false) // detects whether the search button has been pressed
-  const [coinsPerPage, setCoinsPerPage] = useState(20)  // number of coins to show per-page
-  const [pageNum, setPageNum] = useState(1)     // the current page number. 
+  const [coins, setCoins] = useState([])
+  const [search, setSearch] = useState("")
+  const [button, setButton] = useState(false)
+  const [coinsPerPage, setCoinsPerPage] = useState(40)
+  const [pageNum, setPageNum] = useState(1)
 
 
   useEffect(() =>
   {
     let url = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&per_page=${coinsPerPage}&page=${pageNum}`
       axios.get(url).then((response) => {
-        const c = response.data.filter((coin) => coin.id.includes(coinSearch))
+        const c = response.data.filter((coin) => coin.id.includes(search))
         setCoins(c)
       }).catch((error) => {
         console.log(error)
@@ -33,36 +32,18 @@ const Coins = (props) =>
   }, [coinsPerPage, pageNum, button])
 
 
-  // triggers when the user hits enter in the coin search
-  const handleSearchPress = (event) => {
+  const updateSearch = (value) => {
+    setSearch(value)
+  }
+
+  const handleClick = () => {
+    setButton(!button)
+  }
+
+  const handleKey = (event) => {
     if (event.key === "Enter")
     {
       setButton(!button)
-    }
-  }
-
-  //triggers when the user hits enter in the page number form
-  const handlePageNumPress = (event) => {
-    if (event.key === "Enter")
-    {
-      pageNumHandler(Number(pageSearch))
-    }
-  }
-
-  // triggers when the user changes their coins-per-page by clicking an option in the list.
-  const perPageHandler = (number) => {
-    if (number !== coinsPerPage)
-    {
-      setCoinsPerPage(number)
-    }
-  }
-
-  // changes when the user changes their page number by typing a new page number OR when the click the button.
-  const pageNumHandler = (number) => {
-
-    if (number !== Number.NAN && number >= 1 && number < allPages)
-    {
-      setPageNum(number)
     }
   }
 
@@ -73,45 +54,45 @@ const Coins = (props) =>
         <div className='input-wrapper'>
           <input
             placeholder="Search for a coin..."
-            value={coinSearch}
-            onChange={(e) => setCoinSearch(e.target.value) }
-            onKeyPress={(e) => handleSearchPress(e)}
+            value={search}
+            onChange={(e) => updateSearch(e.target.value) }
+            onKeyPress={(e) => handleKey(e)}
           />
 
           <FaSearch
             id="search-icon"
-            onClick={() => {setButton(!button)}}
+            onClick={handleClick}
           />
         </div>
 
         <div className="pagination">
 
           <div className="elements-per-page">
-            <div className="elements-view">
+            <div className="elements-view"> 
               Showing <span>{coinsPerPage}</span> coins per-page
                 <ul className="elements-dropdown">
-                  <li onClick={() => {perPageHandler(5)}}> 5 </li>
-                  <li onClick={() => {perPageHandler(10)}}> 10 </li>
-                  <li onClick={() => {perPageHandler(20)}}> 20 </li>
-                  <li onClick={() => {perPageHandler(50)}}> 50 </li>
-                  <li onClick={() => {perPageHandler(100)}}> 100 </li>
+                  <li> 5 </li>
+                  <li> 10 </li>
+                  <li> 20 </li>
+                  <li> 50 </li>
+                  <li> 100 </li>
                 </ul>
             </div>
           </div>
 
           <div className="change-page-number">
-            <FiChevronsLeft className="chev" onClick={() => pageNumHandler(1)}/>
-            <FiChevronLeft className="chev margin-image" onClick={() => pageNumHandler(pageNum - 1)}/>
+            <FiChevronsLeft />
+            <FiChevronLeft className="margin-image"/>
 
             <input
-              placeholder={"Page: " + pageNum + "/" + allPages }
-              value={pageSearch}
-              onChange={(e) => setPageSearch(e.target.value) }
-              onKeyPress={(e) => handlePageNumPress(e) }
+              placeholder={pageNum + "/" + allPages}
+              value={search}
+              onChange={(e) => updateSearch(e.target.value) }
+              onKeyPress={(e) => handleKey(e)}
             />
 
-            <FiChevronRight className="chev margin-image" onClick={() => pageNumHandler(pageNum + 1)}/>
-            <FiChevronsRight className="chev margin-image" onClick={() => pageNumHandler(allPages - 1)}/>
+            <FiChevronRight className="margin-image"/>
+            <FiChevronsRight className="margin-image"/>
           </div>
 
         </div>
@@ -124,6 +105,14 @@ const Coins = (props) =>
           <p className='hide-mobile'>Volume</p>
           <p className='hide-mobile'>Market Cap</p>
         </div>
+
+        {/*        {coins.map(coin => {
+            return (
+
+            )
+        })}
+
+        */}
 
         {coins.map(coin => {
           return (
